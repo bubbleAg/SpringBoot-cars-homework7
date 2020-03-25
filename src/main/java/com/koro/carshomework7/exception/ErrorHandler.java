1dc795908863fb3ef3c00c1b4dfa1465e9ce4570
Agnieszka Korowacka <ext.agnieszka.korowacka@rail.bombardier.com>
2020-03-25 22:58:39 +0100

    Unused annotation deleted

--- src/main/java/com/koro/carshomework7/exception/ErrorHandler.java
+++ src/main/java/com/koro/carshomework7/exception/ErrorHandler.java
@@ -2,11 +2,9 @@ package com.koro.carshomework7.exception;
 
 import com.koro.carshomework7.controller.ResponseMessage;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestControllerAdvice
-@CrossOrigin
 public class ErrorHandler {
 
     @ExceptionHandler(CarNotFoundException.class)
